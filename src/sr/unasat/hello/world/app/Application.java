a6c96f636c2a7b5bc19291c175e185f41f979f56
Maarten Narain <mnarain@qualogy.com>
2024-04-23 14:19:47 -0300

    booleans en integers

---
+++ src/sr/unasat/hello/world/app/Application.java
@@ -0,0 +1,42 @@
+package sr.unasat.hello.world.app;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //declaring a variable of aanmaken van een variable
+        boolean isValidAnswer;
+
+        System.out.println("is 10 + 0 = 11?");
+
+        //initaliseren van een variable is de eerste keer dat we een waarde toekennen aan een variable
+        isValidAnswer = false; //initializing a variable of initialiseren van een variable
+
+        System.out.println("Het antwoord is niet valide. de variable isValidAnswer is: " + isValidAnswer);
+
+
+        System.out.println("is 10 + 0 = 10?");
+        isValidAnswer = true; //assigning a value to a variable of toekennen van een waarde aan een variable
+        System.out.println("Het antwoord is valide. de variable isValidAnswer is: " + isValidAnswer);
+
+//Zelfde vraagstuk maar dan met variabelen
+        int nummer1 = 10;
+        int nummer2 = 0;
+        System.out.println();
+        System.out.println("zelfde vraagstuk maar dan met variabelen");
+        System.out.println("is 10 + 0 = 11?");
+        isValidAnswer = ((nummer1 + nummer2) == 11);
+        System.out.println("het antwoord is " + isValidAnswer);
+
+         nummer1 = 10;
+         nummer2 = 0;
+        System.out.println();
+        System.out.println("zelfde vraagstuk maar dan met variabelen");
+        System.out.println("is 10 + 0 = 10?");
+        isValidAnswer = ((nummer1 + nummer2) == 10);
+        System.out.println("het antwoord is " + isValidAnswer);
+
+
+    }
+
+}
